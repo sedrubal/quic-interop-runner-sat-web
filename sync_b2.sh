2acc12a09adf35b744c676300467e7376d868eba
sedrubal <dev@sedrubal.de>
2021-09-15 10:43:17 +0200

    Update sync_b2.sh to upload updated index.html files

--- sync_b2.sh
+++ sync_b2.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+set -eux
+
 DIR=logs/
 BUCKET=b2://quic-interop-runner-sat
 
-exec backblaze-b2 sync --compareVersions none --skipNewer "${DIR}" "${BUCKET}/${DIR}"
+backblaze-b2 sync --excludeAllSymlinks --compareVersions none --skipNewer --excludeRegex ".*\.pickle" "${DIR}" "${BUCKET}/${DIR}"
+backblaze-b2 sync --excludeAllSymlinks --compareVersions none --skipNewer --excludeRegex ".*" --includeRegex ".*\.html" "${DIR}" "${BUCKET}/${DIR}"
