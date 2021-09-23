4c0878783ed8a0fd2f43c733c6aa5f8416bffb06
sedrubal <dev@sedrubal.de>
2021-09-23 16:11:08 +0200

    Add i7 sync script

---
+++ sync_i7.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -eux
+
+HOST="faui7s4.informatik.uni-erlangen.de"
+PATH_ON_HOST="/var/www/interop.sedrubal.de/"
+
+exec rsync -avzlxhi --info=progress2 . "${HOST}:${PATH_ON_HOST}"
+# exec rsync -avzlxhi --info=progress2 --zc=zstd "${HOST}:${PATH_ON_HOST}" .
