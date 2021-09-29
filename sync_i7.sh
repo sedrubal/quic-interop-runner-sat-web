1b3596ae6d1728f10c556e9c33a9a0e03ae1eda6
sedrubal <dev@sedrubal.de>
2021-09-29 16:37:49 +0200

    Sync only logs not html/js

--- sync_i7.sh
+++ sync_i7.sh
@@ -3,7 +3,7 @@
 set -eux
 
 HOST="faui7s4.informatik.uni-erlangen.de"
-PATH_ON_HOST="/var/www/interop.sedrubal.de/"
+PATH_ON_HOST="/var/www/interop.sedrubal.de/logs/"
 
-exec rsync -avzlxhi --info=progress2 . "${HOST}:${PATH_ON_HOST}"
+exec rsync -avzlxhi --info=progress2 ./logs/ "${HOST}:${PATH_ON_HOST}"
 # exec rsync -avzlxhi --info=progress2 --zc=zstd "${HOST}:${PATH_ON_HOST}" .
